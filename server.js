15413eef6487fdd7305ef1d77c8d9f37ea257900
Wesley <wesley.henrique.pgm@gmail.com>
2021-02-08 19:50:13 -0300

    inicializando

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-var nome = 0;
-var teste = 1; 
+const express = require('express')
+const server = express()
+
+//rotas
+server.get("/", function(req, res){
+    return res.send("helllo world 3")
+})
+
+server.listen(5000, function(){
+    console.log("server is runing")
+})
