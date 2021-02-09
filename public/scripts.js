95004cc05354fb2f928a5cf42c487cabcd9e0a3c
Wesley <wesley.henrique.pgm@gmail.com>
2021-02-09 15:38:57 -0300

    configurando nunjucks e primeiras rotas

---
+++ public/scripts.js
@@ -0,0 +1,25 @@
+//selecionar o modal-overlay e fazer o close e open
+const modalOverlay = document.querySelector('.modal-overlay') //pega a classe
+const cards = document.querySelectorAll('.card')//pega coleção de valores iguais
+
+for (let card of cards) {
+    // ouvidor de eventos 
+    //Ativar o active
+    card.addEventListener("click", function () {
+        // id do video
+        const videosId = card.getAttribute("id")
+        modalOverlay.classList.add('active') //adicionar a classe
+        // pega o iframe
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videosId}`
+    })
+}
+
+// fechar nn precisa colocar a const 
+// remover o active
+document.querySelector('.close-modal').addEventListener("click", function () {
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src = ""
+
+})
+
+
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+
+//arquivos staticos
+server.use(express.static('public'))
+
+//nunjucks
+server.set("view engine", "njk")
+nunjucks.configure("views", {
+    express: server
+})
 
 //rotas
 server.get("/", function(req, res){
-    return res.send("helllo world 3")
+    return res.render("about")
 })
+
+server.get("/portfolio", function(req, res){
+    return res.render("portfolio")
+})
+
+
 
 server.listen(5000, function(){
     console.log("server is runing")
