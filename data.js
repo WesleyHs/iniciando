af402710957097d8e3fb25c5030e2002d887487a
Wesley <wesley.henrique.pgm@gmail.com>
2021-02-09 16:32:17 -0300

    enviando dados do back pra o front

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "KdgKQoq1jk0",
+        title: "Design UX/UI",
+        duration: "40 minutos",
+        price: "59"
+    },
+    {
+        id: "KdgKQoq1jk0",
+        title: "Design UX/UI",
+        duration: "40 minutos",
+        price: "59"
+    },
+    {
+        id: "KdgKQoq1jk0",
+        title: "Design UX/UI",
+        duration: "40 minutos",
+        price: "59"
+    },
+    {
+        id: "KdgKQoq1jk0",
+        title: "Design UX/UI",
+        duration: "40 minutos",
+        price: "59"
+    },
+    {
+        id: "KdgKQoq1jk0",
+        title: "Design UX/UI",
+        duration: "40 minutos",
+        price: "59"
+    },
+    {
+        id: "KdgKQoq1jk0",
+        title: "Design UX/UI",
+        duration: "40 minutos",
+        price: "59"
+    },
+]
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data") //array do data.js
 
 //arquivos staticos
 server.use(express.static('public'))
@@ -9,16 +10,30 @@ server.use(express.static('public'))
 //nunjucks
 server.set("view engine", "njk")
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 //rotas
 server.get("/", function(req, res){
-    return res.render("about")
+
+    const about = {
+        avatar_url: "https://jpimg.com.br/uploads/2019/04/james-williams-chris.jpg",
+        name: "Cris Rock",
+        role: "Ator",
+        description: 'Todo mundo odeia o Chris. <a href="https://globoplay.globo.com/todo-mundo-odeia-o-chris/t/tDyxRKNSFp/" target="_blank">Assista no Globo play </a>',
+        link: [
+            {name:"Github", url:"https://github.com/WesleyHs"},
+            {name:"Twitter", url:"https://twitter.com/login?lang=pt"},
+            {name:"Linkedin", url:"www.linkedin.com"},
+
+        ]
+    }
+    return res.render("about", {about:about}) //pega o data about
 })
 
 server.get("/portfolio", function(req, res){
-    return res.render("portfolio")
+    return res.render("portfolio", {items:videos})
 })
 
 
